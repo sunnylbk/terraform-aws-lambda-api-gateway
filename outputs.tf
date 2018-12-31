9a93e01005ce2d6e99f72420718d67aac4d2348c
bharath kumar <bharath@bharaths-MacBook-Pro.local>
2018-12-31 11:37:13 -0500

    Added lambda name and arn in output variables

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "lambda_zip" {
   value = "${aws_s3_bucket.lambda_repo.bucket}/${var.lambda_zip_path}"
 }
 
+output "lambda_arn" {
+  value = "${module.lambda.arn}"
+}
+
+output "lambda_name" {
+  value = "${module.lambda.name}"
+}
+
 output "vpc_id" {
   value = "${module.vpc_subnets.vpc_id}"
 }
